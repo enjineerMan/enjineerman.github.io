ef5666c6a4d7030bf2d2e39a7d1b65a725cea8b3
enjineerMan <david.wang236@gmail.com>
2022-01-29 19:33:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState, useEffect} from "react";
 function App() {
+  const [linkedinColor, setLinkedinColor] = useState("black");
+  const [githubColor, setGithubColor] = useState("black");
+  const [resumeColor, setResumeColor] = useState("black");
+  var handleEnter_L = function(event){
+    //setLinkedinColor("rgb(48, 229, 252)");
+    setLinkedinColor("rgb(78, 234, 245)")
+  }
+
+  var handleLeave_L = function(){
+    setLinkedinColor("black");
+  }
+  var handleEnter_G = function(event){
+    setGithubColor("rgb(78, 234, 245)");
+  }
+
+  var handleLeave_G = function(){
+    setGithubColor("black");
+  }
+  var handleEnter_R = function(event){
+    setResumeColor("rgb(78, 234, 245)");
+  }
+
+  var handleLeave_R = function(){
+    setResumeColor("black");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="div">
+        <h1 className="header"> David Wang </h1>
+
+        <a 
+        className="linkedin" 
+        href={"https://www.linkedin.com/in/david-wang-3abab7189/"}
+        onMouseEnter={handleEnter_L} 
+        onMouseLeave={handleLeave_L}
+        target="_blank"
+        style={{textDecoration: "none", color: linkedinColor, fontWeight: linkedinColor === "black" ? "normal" : "600"}}
+        > 
+          Linkedin 
         </a>
-      </header>
+
+        <a 
+        className="github" 
+        href={"https://github.com/enjineerMan"}
+        onMouseEnter={handleEnter_G}
+        onMouseLeave={handleLeave_G}
+        target="_blank"
+        style={{textDecoration: "none", color:githubColor, fontWeight:githubColor === "black" ? "normal" : "600"}}
+        > 
+          Github 
+        </a>
+
+        <a 
+        className="resume" 
+        href={"David_Wang.pdf"}
+        download
+        onMouseEnter={handleEnter_R}
+        onMouseLeave={handleLeave_R}
+        target="_blank"
+        style={{textDecoration: "none", color:resumeColor, fontWeight:resumeColor === "black" ? "normal" : "600"}}
+        > 
+          Resume 
+        </a>
+
+      </div>
     </div>
   );
 }
